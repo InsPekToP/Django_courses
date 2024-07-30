53c73d558f8d6f5ad686e0b8c9462f3a1a5014d9
IsPekToP <inspektop1@gmail.com>
2024-07-30 18:51:26 +0200

    добавил 18main2_1

--- 18main1_1/instruction.py
+++ 18main1_1/instruction.py
@@ -1,11 +1,4 @@
 #18main1_1
-#Создание проекта(актавиция venv)
+#Настройка проекта.Модель с курсами
 
-#pip install virtualenv - скачивание библиотеки для venv
-
-#virtualenv venv - создание папки с venv
-#venv\Scripts\activate - активировать venv
-#pip freeze - проверка установленных библиотек
-#deactivate - деактивировать venv
-#pip install django - установка Django
-#pip freeze > requirements.txt - установка зависимостей
+#
